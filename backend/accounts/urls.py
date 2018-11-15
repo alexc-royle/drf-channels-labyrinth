5a84496c0f638226d0b29264fbe81eeb291e5bde
Alex <alex@gomo>
2018-11-15 16:20:16 +0000

    Added a view and urls for user management

---
+++ backend/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.UserCreate.as_view(), name='account-create'),
+]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from . import serializers
+
+class UserCreate(APIView):
+    """
+    Creates the User
+    """
+
+    def post(self, request, format='json'):
+        return Response('hello')
