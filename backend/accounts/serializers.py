c53032b782fa657e5ce82b47917e6d25db18f3d8
Alex <alex@gomo>
2018-11-15 15:53:26 +0000

    Adding user serializer and related tests

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(min_length=0)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password']
+        )
+        return user
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class AccountsTest(APITestCase):
+    def setup(self):
+        self.test_user = User.objects.create_user('test_user', 'test@example.com', 'testpassword')
+        self.create_url = reverse('account-create')
+
+    def test_create_user(self):
+        """
+        Ensure we can create a new user and a valid token is associated with itself.
+        """
+        data = {
+            'username': 'foobar',
+            'email': 'foobar@example.com',
+            'password': 'somepassword'
+        }
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['username'], data['username'])
+        self.assertEqual(response.data['email'], data['email'])
+        self.assertFalse('password' in response.data)
