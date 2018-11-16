a5397c56a324ead4594c111afe8f81e91d007067
Alex <alex@gomo>
2018-11-16 15:53:21 +0000

    Adding token creation and adding it to the response for newly created users

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -16,6 +17,8 @@ class UserCreate(APIView):
         if serializer.is_valid():
             user = serializer.save()
             if user:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                token = Token.objects.get(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
