3aa3b9cf752995151d627f4656b8a49c173cfb34
Alex <alex@gomo>
2018-11-19 08:57:24 +0000

    Tidying up and giving account creation a different url

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.UserCreate.as_view(), name='account-create'),
+    url(r'^create$', views.UserCreate.as_view(), name='account-create'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import permissions
-from rest_framework import status
+from rest_framework import permissions, status
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
