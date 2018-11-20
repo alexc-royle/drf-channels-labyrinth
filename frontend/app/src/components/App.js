641aaefa1aded417a0504939ca99055395e1c10b
Alex <alex@gomo>
2018-11-20 09:55:12 +0000

    Playing around with react router

--- frontend/app/src/components/App.js
+++ frontend/app/src/components/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import LoginForm from '../containers/LoginForm'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import LoginForm from '../containers/LoginForm';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <LoginForm />
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <PrivateRoute path="/" exact component={Home} />
+            <Route path="/login/" component={Login} />
+            <Route path="/register/" component={Register} />
+            <Route component={NoPathMatch} />
+          </Switch>
+          <LoginForm />
+        </div>
+      </Router>
     );
   }
 }
+const Home = () => <h2>Home</h2>;
+const Login = () => <h2>Login</h2>;
+const Register = () => <h2>Register</h2>;
+const NoPathMatch = () => <h2>No Match</h2>;
+
+const PrivateRoute = ({ component: Component, ...rest}) => (
+  <Route {...rest} render={(props) => (
+    false === true ? <Component {...props} /> : <Redirect to='/login' />
+  )} />
+);
 
 export default App;
